bb3277d35c9880884dfa401600010bef4115030b
Daniel Gomes <daniel.gomes@provafacilnaweb.com.br>
2020-03-07 19:46:05 -0300

    Remoção de código e correção de script

--- app.js
+++ app.js
@@ -21,16 +21,4 @@ consign()
   .then('src/controllers')
   .into(app);
 
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 module.exports = app;
